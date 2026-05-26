a0a9016777083303e5170c0e2fbcb4be4815bfe0
LordLyam77 <lyamfernandes306@gmail.com>
2026-05-26 17:41:56 +0530

    Fixed auth page styling7

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,27 +1,38 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
-    </div>
+@extends('layouts.app')
 
-    <form method="POST" action="{{ route('password.confirm') }}">
+@section('content')
+
+<div class="card">
+
+    <h2>Confirm Password</h2>
+
+    <p style="margin-top:10px;">
+        Please confirm your password before continuing.
+    </p>
+
+    @if ($errors->any())
+        <div class="success" style="background:#dc2626;">
+            {{ $errors->first() }}
+        </div>
+    @endif
+
+    <form method="POST"
+          action="{{ route('password.confirm') }}">
+
         @csrf
 
-        <!-- Password -->
-        <div>
-            <x-input-label for="password" :value="__('Password')" />
+        <input type="password"
+               name="password"
+               placeholder="Password"
+               required>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+        <button type="submit"
+                class="btn btn-primary">
+            Confirm Password
+        </button>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <div class="flex justify-end mt-4">
-            <x-primary-button>
-                {{ __('Confirm') }}
-            </x-primary-button>
-        </div>
     </form>
-</x-guest-layout>
+
+</div>
+
+@endsection
